ce14d37d8125024c15f31178068bc450bfc074e5
SSaajan <50995007+SSaajan@users.noreply.github.com>
2022-12-20 08:58:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    user: 'saajansuraj@gmail.com',
+    clientId: '822783528470-fniicjffrtkg6voot6986iislpfvro0h.apps.googleusercontent.com',
+    clientSecret: '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',
+    refreshToken: '1//047CjK722BAGqCgYIARAAGAQSNwF-L9IrFg3WohT98Q8g1gcPUhZGUYx_mPY1tUsWaqac8dO8Ze4uGBxoozC-kYztMO7vQIcPkqM'
+}
